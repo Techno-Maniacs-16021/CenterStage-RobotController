9665ea7c5bf200f01035e3fc92a4d99cbe0225e3
Infant Elfrick Gnanasusairaj <infant.elfrick@gmail.com>
2023-11-18 07:53:39 -0500

    fml

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Rick_Driver_Mode.java
@@ -0,0 +1,160 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import static android.os.SystemClock.sleep;
+import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_WITHOUT_ENCODER;
+import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.STOP_AND_RESET_ENCODER;
+
+import android.widget.HorizontalScrollView;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.PwmControl;
+import com.qualcomm.robotcore.hardware.ServoImplEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+
+@Config
+@TeleOp
+public class Rick_Driver_Mode extends OpMode
+{
+    /////////////////////////////////////////////
+    ServoImplEx intake_arm,claw,claw_angler;
+    CRServo left_aligner, right_aligner;
+    DcMotorEx left_slides,right_slides,left_intake,right_intake;
+    AnalogInput claw_Position, claw_Angle;
+    /////////////////////////////////////////////
+    private ElapsedTime loopTime = new ElapsedTime();
+    private MecanumDrive drive;
+    public static double clawPosition,clawAngle,intakePower,speedMultiplier;
+    boolean intaked,outaked,OVERIDE;
+    public static int HIGH,MID;
+    public double slidePower;
+    /////////////////////////////////////////////
+    @Override
+    public void init(){
+////////////////////////HARDWARE INIT////////////////
+        drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
+
+        intake_arm = hardwareMap.get(ServoImplEx.class, "IA");
+        claw = hardwareMap.get(ServoImplEx.class, "Claw");
+        claw_angler = hardwareMap.get(ServoImplEx.class,"CA");
+
+        left_aligner = hardwareMap.get(CRServo.class,"LA");
+        right_aligner = hardwareMap.get(CRServo.class,"RA");
+
+        claw_Position = hardwareMap.get(AnalogInput.class,"Claw Pos");
+        claw_Angle = hardwareMap.get(AnalogInput.class,"Claw Angle");
+
+        left_slides = hardwareMap.get(DcMotorEx.class,"LS");
+        right_slides = hardwareMap.get(DcMotorEx.class,"RS");
+        left_intake = hardwareMap.get(DcMotorEx.class,"LI");
+        right_intake = hardwareMap.get(DcMotorEx.class,"RI");
+
+////////////////////////SET PWM RANGE////////////////
+        intake_arm.setPwmRange(new PwmControl.PwmRange(500,2500));
+        claw.setPwmRange(new PwmControl.PwmRange(500,2500));
+        claw_angler.setPwmRange(new PwmControl.PwmRange(500,2500));
+////////////////////////HARDWARE REVERSING///////////
+left_slides.setDirection(DcMotorSimple.Direction.REVERSE);
+right_intake.setDirection(DcMotorSimple.Direction.REVERSE);
+////////////////////////MOTOR BRAKE BEHAVIOR/////////
+        left_slides.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        right_slides.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+////////////////////////ENCODER RESET////////////////
+        left_slides.setMode(RUN_WITHOUT_ENCODER);
+        right_slides.setMode(RUN_WITHOUT_ENCODER);
+////////////////////////DASHBOARD TELEMETRY//////////
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+////////////////////////STATUS UPDATE////////////////
+        telemetry.addData("Status", "Initialized");
+/////////////////////////////////////////////////////
+        slidePower=0;
+
+    }
+    @Override
+    public void init_loop(){
+
+    }
+    @Override
+    public void start(){
+
+    }
+    @Override
+    public void loop(){
+//INTAKE
+        if(gamepad1.a){
+            left_intake.setPower(0.5);
+            right_intake.setPower(0.5);
+            left_aligner.setPower(1);
+            right_aligner.setPower(1);
+        }
+        else if (gamepad1.b){
+            left_intake.setPower(-1);
+            right_intake.setPower(-1);
+            left_aligner.setPower(0);
+            right_aligner.setPower(0);
+        }
+        else {
+            left_intake.setPower(0);
+            right_intake.setPower(0);
+            left_aligner.setPower(0);
+            right_aligner.setPower(0);
+        }
+        if(gamepad1.dpad_up)intake_arm.setPosition(1);
+        else if(gamepad1.dpad_down)intake_arm.setPosition(0);
+//Slides
+        left_slides.setPower(slidePower);
+        right_slides.setPower(slidePower);
+        if(gamepad1.right_trigger!=0) slidePower = gamepad1.right_trigger;
+        else if(gamepad1.left_trigger!=0) slidePower = -gamepad1.left_trigger;
+        else slidePower = 0;
+//Outtake
+        if(gamepad1.left_bumper)claw_angler.setPosition(1);
+        else if(gamepad1.right_bumper){
+            claw_angler.setPosition(0);
+            claw.setPosition(0.5);
+        }
+        if(gamepad1.x)claw.setPosition(1);
+        else if(gamepad1.y)claw.setPosition(0);
+////////////////////////DRIVE LOGIC//////////////////
+
+        drive.setDrivePowers(new PoseVelocity2d(
+                new Vector2d(
+                        -gamepad1.left_stick_y ,
+                        -gamepad1.left_stick_x
+                ),
+                -gamepad1.right_stick_x
+        ));
+
+        drive.updatePoseEstimate();
+        telemetry.addLine("Running: TechnoManiacs CenterStage Operating System - V0.1");
+
+////////////////////////TELEMETRY////////////////////
+        telemetry.addData("loop time: ",loopTime.time());
+        telemetry.addData("left slide pos:",left_slides.getCurrentPosition());
+        telemetry.addData("claw position: ",clawPosition);
+        telemetry.addData("claw angle: ",claw);
+        loopTime.reset();
+        telemetry.update();
+    }
+    @Override
+    public void stop(){
+
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Rick_Driver_Mode_Reference.java
@@ -0,0 +1,302 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import static android.os.SystemClock.sleep;
+import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_WITHOUT_ENCODER;
+import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.STOP_AND_RESET_ENCODER;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.PwmControl;
+import com.qualcomm.robotcore.hardware.ServoImplEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+
+@Config
+@TeleOp
+public class Rick_Driver_Mode_Reference extends OpMode
+{
+    /////////////////////////////////////////////
+    ServoImplEx left_arm, right_arm, wrist;
+    CRServo left_intake, right_intake;
+    DcMotorEx horizontal_slides, left_vertical_slides,right_vertical_slides;
+    AnalogInput rightArmPosition,leftArmPosition;
+    /////////////////////////////////////////////
+    private ElapsedTime armCooldown = new ElapsedTime();
+    private ElapsedTime loopTime = new ElapsedTime();
+    private PIDController hController,vController;
+    private MecanumDrive drive;
+    public static double hp,hi,hd,hTarget;
+    public static double vp,vi,vd,vf,vTarget;
+    public static boolean farmingMode = true;
+    public static boolean active = true;
+    public static double armPosition,wristPosition,intakePower,speedMultiplier;
+    boolean intaked,outaked,OVERIDE;
+    public static int HIGH,MID;
+    /////////////////////////////////////////////
+    @Override
+    public void init(){
+////////////////////////HARDWARE INIT////////////////
+        drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
+        left_arm = hardwareMap.get(ServoImplEx.class, "LA");
+        right_arm = hardwareMap.get(ServoImplEx.class, "RA");
+        wrist = hardwareMap.get(ServoImplEx.class,"Wrist");
+        left_intake = hardwareMap.get(CRServo.class,"LI");
+        right_intake = hardwareMap.get(CRServo.class,"RI");
+        rightArmPosition = hardwareMap.get(AnalogInput.class,"rArmPos");
+        leftArmPosition = hardwareMap.get(AnalogInput.class,"lArmPos");
+        left_vertical_slides = hardwareMap.get(DcMotorEx.class,"LV");
+        right_vertical_slides = hardwareMap.get(DcMotorEx.class,"RV");
+        horizontal_slides = hardwareMap.get(DcMotorEx.class,"H");
+////////////////////////SET PWM RANGE////////////////
+        left_arm.setPwmRange(new PwmControl.PwmRange(500,2500));
+        right_arm.setPwmRange(new PwmControl.PwmRange(500,2500));
+        wrist.setPwmRange(new PwmControl.PwmRange(510,2490));
+////////////////////////HARDWARE REVERSING///////////
+        right_arm.setDirection(ServoImplEx.Direction.REVERSE);
+        left_intake.setDirection(CRServo.Direction.REVERSE);
+        left_vertical_slides.setDirection(DcMotorSimple.Direction.REVERSE);
+////////////////////////MOTOR BRAKE BEHAVIOR/////////
+        left_vertical_slides.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        right_vertical_slides.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        horizontal_slides.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+////////////////////////ENCODER RESET////////////////
+        left_vertical_slides.setMode(RUN_WITHOUT_ENCODER);
+        right_vertical_slides.setMode(RUN_WITHOUT_ENCODER);
+        horizontal_slides.setMode(RUN_WITHOUT_ENCODER);
+////////////////////////PID CONTROLLERS//////////////
+        hController = new PIDController(hp,hi,hd);
+        vController = new PIDController(vp,vi,vd);
+////////////////////////DASHBOARD TELEMETRY//////////
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+////////////////////////DRIVE INIT///////////////////
+
+////////////////////////STATUS UPDATE////////////////
+        telemetry.addData("Status", "Initialized");
+/////////////////////////////////////////////////////
+        vp=0.011;vi=0;vd=0.00005;vf=0.015;vTarget = 0;hp=0.0035;hi=0;hd=0.000;hTarget = 0;speedMultiplier=1;
+        armPosition = 0.5;wristPosition = 0;intakePower=0;
+        HIGH = 930;MID= 00;
+        intaked = false; outaked=false; farmingMode = false; OVERIDE = false; active=true;
+
+    }
+    @Override
+    public void init_loop(){
+
+    }
+    @Override
+    public void start(){
+        left_arm.setPosition(armPosition);
+        right_arm.setPosition(armPosition);
+        wrist.setPosition(wristPosition);
+        left_intake.setPower(intakePower);
+        right_intake.setPower(intakePower);
+    }
+    @Override
+    public void loop(){
+////////////////////////HORIZONTAL SLIDES PID////////
+        hController.setPID(hp,hi,hd);
+        int hPos = horizontal_slides.getCurrentPosition();
+        double hPID = hController.calculate(hPos,hTarget);
+        double hPower = hPID;
+        if(!gamepad2.b)horizontal_slides.setPower(hPower);
+////////////////////////VERTICAL SLIDES PID//////////
+        vController.setPID(vp,vi,vd);
+        int vPos = (right_vertical_slides.getCurrentPosition()+left_vertical_slides.getCurrentPosition())/2;
+        double vPID = vController.calculate(vPos,vTarget);
+        double vPower = vPID+vf;
+        if(!gamepad2.b){
+            left_vertical_slides.setPower(-verticalPID(vTarget, vController, left_vertical_slides.getCurrentPosition()));
+            right_vertical_slides.setPower(verticalPID(vTarget, vController, right_vertical_slides.getCurrentPosition()));
+        }
+////////////////////////ARM ANG VARS/////////////////
+        double leftArmAngle = ((leftArmPosition.getVoltage()-0.82) /3.3) * 360;
+        double rightArmAngle = ((2.499-rightArmPosition.getVoltage())/3.3) * 360;
+        double armAngle = (leftArmAngle+rightArmAngle)/2;
+////////////////////////SERVO POSITIONS//////////////
+        left_arm.setPosition(armPosition);
+        right_arm.setPosition(armPosition);
+        wrist.setPosition(wristPosition);
+        left_intake.setPower(intakePower);
+        right_intake.setPower(intakePower);
+////////////////////////FARMING MODE/////////////////
+        if(gamepad2.a||farmingMode){
+            //loop starter or breaker
+            if(gamepad1.dpad_up) {
+                OVERIDE = true;
+                intaked = false;
+                outaked = false;
+            }
+            else OVERIDE = false;
+            if (gamepad1.dpad_up) {
+                wristPosition = 0;
+                armPosition = 0.5;
+                intakePower = 0;
+            } else if (gamepad1.dpad_left) {
+                armPosition = 0.0;
+            }
+            if(!OVERIDE) {
+                if (armAngle < 20 && gamepad1.a) {
+                    hTarget += 50;
+                    intakePower = 1;
+                }
+                if (horizontal_slides.getCurrentPosition() > 25 && !gamepad1.a) {
+                    intakePower = 0;
+                    armPosition = .6;
+                    wristPosition = 1;
+                    hTarget = 0;
+                    intaked = true;
+                }
+                if (intaked && getError(horizontal_slides.getCurrentPosition(), hTarget) < 10) {
+                    armPosition = 0.9;
+                    if (armAngle > 158) intakePower = -1;
+                    if (vTarget == HIGH && getError(vPos, vTarget) < 10) {
+                        vTarget = 0;
+                        intaked = false;
+                        outaked = true;
+                    }
+                }
+                if (outaked) {
+                    armPosition = 0.01;
+                    intakePower = 0;
+                    wristPosition = 0;
+                    outaked = false;
+                }
+            }
+
+        }
+////////////////////////MANUAL MODE//////////////////
+        else if(gamepad2.b){
+            horizontal_slides.setPower(-gamepad1.left_stick_x);
+            left_vertical_slides.setPower(-gamepad1.right_stick_y);
+            right_vertical_slides.setPower(-gamepad1.right_stick_y);
+            if(gamepad1.a)intakePower=1;
+            else if(gamepad1.b)intakePower=-1;
+            else intakePower=0;
+            if(gamepad1.dpad_left)wristPosition=0;
+            else if(gamepad1.dpad_right)wristPosition=1;
+            if(gamepad1.left_bumper&&armCooldown.time()>0.05&&armPosition<1){
+                armPosition+=0.025;
+                armCooldown.reset();
+            }
+            else if(gamepad1.left_bumper&&armCooldown.time()>0.05&&armPosition>0){
+                armPosition-=0.025;
+                armCooldown.reset();
+            }
+        }
+////////////////////////RESET MODE///////////////////
+        else if(gamepad2.left_trigger>0&&gamepad2.right_trigger>0&&gamepad1.left_trigger>0&&gamepad1.right_trigger>0){
+            left_vertical_slides.setPower(-1);
+            right_vertical_slides.setPower(-1);
+            horizontal_slides.setPower(-1);
+            if(gamepad1.a){
+                left_vertical_slides.setMode(STOP_AND_RESET_ENCODER);
+                right_vertical_slides.setMode(STOP_AND_RESET_ENCODER);
+                horizontal_slides.setMode(STOP_AND_RESET_ENCODER);
+                left_vertical_slides.setMode(RUN_WITHOUT_ENCODER);
+                right_vertical_slides.setMode(STOP_AND_RESET_ENCODER);
+                horizontal_slides.setMode(RUN_WITHOUT_ENCODER);
+            }
+        }
+////////////////////////CIRCUIT MODE/////////////////
+        else if(active){
+            if(gamepad1.dpad_up) {
+                OVERIDE = true;
+                intaked = false;
+            }
+            else OVERIDE = false;
+            if (gamepad1.dpad_up) {
+                wristPosition = 0;
+                armPosition = 0.5;
+                intakePower = 0;
+            } else if (gamepad1.dpad_left) {
+                armPosition = 0.2 ;
+                wristPosition = 0;
+            }
+            if(!OVERIDE) {
+                if (armAngle < 40 && gamepad1.a) {
+                    hTarget += 50;
+                    intakePower = 1;
+                    if(horizontal_slides.getCurrentPosition()>350)armPosition=0.01;
+
+                }
+                if (horizontal_slides.getCurrentPosition() > 25 && !gamepad1.a) {
+                    intakePower = .5;
+                    armPosition = .6;
+                    wristPosition = 1;
+                    hTarget = 0;
+                    horizontal_slides.setPower(-.5);
+                    intaked = true;
+                }
+                if (intaked && getError(horizontal_slides.getCurrentPosition(), hTarget) < 25) {
+                    armPosition = 0.8;
+                    if (armAngle > 145) {
+                        sleep(75);
+                        intakePower = -.5;
+                    }
+
+                    if (vTarget > 0 && getError(vPos, vTarget) < 10||(getError(vPos, vTarget) < 50&&gamepad1.y)) {
+                        sleep(75);
+                        vTarget = 0;
+                        armPosition=0.5;
+                        wristPosition=0;
+                        intakePower=0;
+                        intaked = false;
+                    }
+                }
+            }
+
+        }
+////////////////////////DRIVE LOGIC//////////////////
+
+        drive.setDrivePowers(new PoseVelocity2d(
+                new Vector2d(
+                        -gamepad1.left_stick_y ,
+                        -gamepad1.left_stick_x
+                ),
+                -gamepad1.right_stick_x
+        ));
+
+        drive.updatePoseEstimate();
+        telemetry.addLine("Running: TechnoManiacs CenterStage Operating System - V0.1");
+
+////////////////////////TELEMETRY////////////////////
+        telemetry.addData("horizontal position: ",hPos);
+        telemetry.addData("horizontal target position: ",hTarget);
+        telemetry.addData("horizontal power: ",hPower);
+        telemetry.addData("vertical position: ",vPos);
+        telemetry.addData("vertical target position: ",vTarget);
+        telemetry.addData("vertical power: ",vPower);
+        telemetry.addData("arm angle:", armAngle);
+        telemetry.addData("loop time: ",loopTime.time());
+        loopTime.reset();
+        telemetry.update();
+    }
+    @Override
+    public void stop(){
+
+    }
+    public static int getError(int current, double target){
+        int error = Math.abs((int)target-current);
+        return error;
+    }
+    public static double verticalPID(double target, PIDController pid,int pos){
+        ////////////////////////VERTICAL SLIDES PID//////////
+        pid.setPID(vp,vi,vd);
+        double vPID = pid.calculate(pos,target);
+        double vPower = vPID+vf;
+        return vPower;
+    }
+}
